f62368442276a4e61b3b0994c3d55f90f6e6dbf0
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2020-10-08 22:34:02 +0900

    feat: Implement the function that decides line width

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
 
 canvas.width = 700;
 canvas.height = 700;
@@ -36,6 +37,11 @@ function handleColorClick(event) {
     ctx.strokeStyle = color;
 }
 
+function handleRangeChange(event) {
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
@@ -43,4 +49,12 @@ if (canvas) {
     canvas.addEventListener("mouseleave", stopPainting);
 }
 
-Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
+if (colors) {
+    Array.from(colors).forEach(color => 
+        color.addEventListener("click", handleColorClick)
+    );
+}
+
+if (range) {
+    range.addEventListener("input", handleRangeChange);
+}
